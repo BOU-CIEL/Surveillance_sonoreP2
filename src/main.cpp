464a407ba983194cc74ab6208ff1fa3393b13128
adel <adelbouhafs621@gmail.com>
2025-04-17 16:48:04 +0200

    commit#6

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,22 @@
 SSD1306 ecranOled(0x3c, 5, 4);
 
 void setup() {
+  Serial.begin(9600);
+
   ecranOled.init();
   ecranOled.clear();
   ecranOled.setFont(ArialMT_Plain_16);
   ecranOled.drawString(0, 10, "Mon appli v1.0");
   ecranOled.display();
+
+  son.Setup();
+  
 }
 
 void loop() {
-  Serial.println("Bonjour!");
-  sleep(2);
+  son.SamplesDmaAcquisition();
+  Serial.print("niveauSonoreMoyen");
+  Serial.println(son.niveauSonoreMoyen);
+  Serial.print("niveauSonoreCrete");
+  Serial.println(son.niveauSonoreCrete);
 }
